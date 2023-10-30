a4da96e1bef6da129a37e823933303f6a9f3de22
DESKTOP-OVUED56\SBLP10 <kamrul.singularity@gmail.com>
2023-10-30 17:55:17 +0600

    develop one to one relationship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
+use App\Models\Owner;
 
 class HomeController extends Controller
 {
     public function showDataTable(){
+
+        $car = Owner::find(1)->car;
+
+        dd($car);
+
         return view('home.index');
     }
 }
