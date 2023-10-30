f63ede3f39e1b2f2ad60ca4beb4c057bbe90a089
DESKTOP-OVUED56\SBLP10 <kamrul.singularity@gmail.com>
2023-10-30 11:25:03 +0600

    initial commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('mytitle', 'Eloquent Relationship')
+
+@section('bodyContent')
+
+<div class="d-flex justify-content-between mt-3">
+    <h2>Data Table</h2>
+    {{-- <a href="" class="btn btn-outline-primary">Add New</a> --}}
+</div>
+<table class="mt-3 table table-bordered">
+  <thead>
+    <tr>
+      <th class="col">Name</th>
+      <th class="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    
+  </tbody>
+</table>
+
+
+
+@endsection
