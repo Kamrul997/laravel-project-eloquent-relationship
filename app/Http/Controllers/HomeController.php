bac9d324b0ec3d9d2e951d989be25bf031f93375
DESKTOP-OVUED56\SBLP10 <kamrul.singularity@gmail.com>
2023-10-31 12:18:56 +0600

    create one to many relationship between post and comment table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Car;
 use App\Models\Owner;
+use App\Models\Post;
+use App\Models\Comment;
 
 class HomeController extends Controller
 {
     public function showDataTable(){
 
         $owners = Owner::all();
-        $data = compact('owners');
+        $posts = Post::with('comments')->get();
+
+        $data = compact('owners', 'posts');
         return view('home.index')->with($data);
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,21 +11,48 @@
 <table class="mt-3 table table-bordered">
   <thead>
     <tr>
-      <th class="col">Name</th>
-      <th class="col">Email</th>
+      <th class="col">Owner Name</th>
+      <th class="col">Owner Age</th>
+      <th class="col">Car Name</th>
+      <th class="col">Car Brand</th>
     </tr>
   </thead>
   <tbody>
     @foreach ($owners as $owner)
       <tr>
         <td>{{$owner->name}}</td>
+        <td>{{$owner->age}}</td>
         <td>{{$owner->car->name}}</td>
+        <td>{{$owner->car->brand}}</td>
       </tr>
     @endforeach
     
   </tbody>
 </table>
 
+<div class="row mt-3">
+  @foreach ($posts as $post)
+    <div class="card m-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{$post->post_title}}</h5>
+        <p class="card-text">{{ $post->post_details }}</p>
+        <h6 class="card-title">Comments:</h6>
+        <div>
+          <ul class="list-group list-group-flush">
+            @foreach ($post->comments as $comment)
+              <div class="">
+                <div class="card-body">
+                  <h6 class="font-weight-bold">{{$comment->commentator_name}}</h6>
+                  <small class="text">{{ $comment->comment }}</small>
+                </div>
+              </div>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+    </div>
+  @endforeach
+</div>
 
 
 @endsection
